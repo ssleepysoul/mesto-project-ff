075729bdc79d73209bb33e31a3141a4601bb0cfc
Anastasiia Mikhailova <ssleepysoul@gmail.com>
2024-06-14 09:58:16 +0300

    fixed remarks

--- scripts/index.js
+++ scripts/index.js
@@ -8,20 +8,23 @@
 
 // @todo: Вывести карточки на страницу
 
+const cardList = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
+
+
 function addCard (cardData, deleteCardFn) {
-  const cardList = document.querySelector('.places__list');
-  const cardTemplate = document.querySelector('#card-template').content;
   const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
   const cardTitle = cardElement.querySelector('.card__title');
   cardTitle.textContent = cardData.name;
   const cardImage = cardElement.querySelector('.card__image');
   cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
   const buttonDelete = cardElement.querySelector('.card__delete-button');
 
   buttonDelete.addEventListener('click', function () {
     deleteCardFn(cardElement);
   });
-  cardList.append(cardElement);
+  return cardElement;
 }
 
 function deleteCard (cardElement) {
@@ -29,7 +32,8 @@ function deleteCard (cardElement) {
 }
 
 initialCards.forEach(function (item) {
-  addCard(item, deleteCard);
+  const cardElement = addCard(item, deleteCard);
+  cardList.append(cardElement);
 });
 
 
